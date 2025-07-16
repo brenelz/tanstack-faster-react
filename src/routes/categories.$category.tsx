38ba7aaff80f3936d0a514a9923d324b6aa9f53a
Brenley Dueck <brenleydueck@gmail.com>
2025-07-16 18:54:41 -0500

    Don't await category promise

--- src/routes/categories.$category.tsx
+++ src/routes/categories.$category.tsx
@@ -1,18 +1,18 @@
 import { } from "@tanstack/react-router";
 import { Link } from "@tanstack/react-router";
 import { getCategory } from "@/lib/server";
-import { Product } from "@/db/schema";
 import { preloadImageIds } from "@/lib/imagePreloader";
+import { use } from "react";
 
 export const Route = createFileRoute({
   component: CategoryPage,
   loader: async ({ params }) => {
-    const category = await getCategory({ data: { slug: params.category } });
+    const categoryPromise = getCategory({ data: { slug: params.category } });
 
-    preloadImageIds(category.products.map(product => product.id), 48)
+    categoryPromise.then(category => preloadImageIds(category.products.map(product => product.id), 48))
 
     return {
-      category
+      categoryPromise
     };
   },
   staleTime: 1000 * 60 * 5, // 5 minutes
@@ -20,18 +20,20 @@ export const Route = createFileRoute({
 
 function CategoryPage() {
   const data = Route.useLoaderData();
+  const category = use(data.categoryPromise);
 
-  if (!data.category) {
+  if (!category) {
     return <div>Category not found</div>
   }
 
   return (
     <div className="w-full space-y-8">
-      <h1 className="text-2xl font-bold text-[#FF6B00]">{data.category.name}</h1>
+      <h1 className="text-2xl font-bold text-[#FF6B00]">{category.name}</h1>
       <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4">
-        {data.category.products.map(product => (
+        {category.products.map(product => (
           <Link
             to="/products/$product"
+            key={product.id}
             params={{
               product: product.slug,
             }}
